93999872ed861183b02e97dca87d4abdf2c059e3
one-true-name <32922573+psiburg@users.noreply.github.com>
2020-03-22 14:22:24 -0700

    some lab scene setup, dialogue files added

---
+++ Assets/Scripts/LabSceneController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class LabSceneController : MonoBehaviour
+{
+	private DialogueController DialogueController;
+	public RigidbodyFirstPersonController fpsplayer;
+	[Space]
+	[Space]
+	public TextAsset openingDialog;
+
+	void Start()
+    {
+		StartCoroutine(DoTheEverything());
+	}
+
+	private IEnumerator DoTheEverything()
+	{
+		//opening dialog
+		yield return new WaitForSeconds(0.2f);
+
+		DialogueController = FindObjectOfType<DialogueController>();
+		yield return WaitForDialog(openingDialog);
+	}
+
+	private IEnumerator WaitForDialog(TextAsset asset)
+	{
+		DialogueController.HandleDialogueText(asset);
+		yield return new WaitUntil(() => fpsplayer.enabled);
+	}
+}
