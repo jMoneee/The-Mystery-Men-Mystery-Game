f43c4a20bffc9ed046da9b0b89a6f0eb77b50c05
one-true-name <32922573+psiburg@users.noreply.github.com>
2020-03-30 18:38:59 -0700

    forgot two files

---
+++ Assets/Scripts/SoundOnCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundOnCollision : MonoBehaviour
+{
+	[SerializeField] AudioSource sound;
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		sound.Play();
+	}
+}
