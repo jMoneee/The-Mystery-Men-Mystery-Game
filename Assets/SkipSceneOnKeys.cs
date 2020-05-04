1d66740616eeecd1cd66dfcbbe1c2f33680e59b2
Trenton Gailey <trenton.gailey@gmail.com>
2020-05-04 16:45:27 -0700

    Skip Scene Keybind

---
+++ Assets/SkipSceneOnKeys.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkipSceneOnKeys : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            if (Input.GetKey(KeyCode.O) && Input.GetKeyDown(KeyCode.P))
+            {
+                if (SceneManager.GetActiveScene().buildIndex < SceneManager.sceneCountInBuildSettings - 1)
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                }
+                else
+                {
+                    Cursor.visible = true;
+                    Cursor.lockState = CursorLockMode.None;
+                    SceneManager.LoadScene(0);
+                }
+            }
+        }
+    }
+}
